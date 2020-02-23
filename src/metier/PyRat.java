0b8f9fb344ffe983f00d286dfbda410c28ff96a4
AlexandreNogueiraMendonca <alexandre.nogueira-mendonca@etu.hesge.ch>
2020-02-23 13:30:15 +0100

    deuxieme jet

--- src/metier/PyRat.java
+++ src/metier/PyRat.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class PyRat {
 
     private List<Point> cheeses;
-    private Set<Point> setCheeses;
-    private boolean[][] matrice;
+    private boolean[][] matriceFromages;
+    private boolean [][] matriceAtteint;
     private  List<Point> pointAccessible;
     private Map<Point, List<Point>> labyComplet;
 
@@ -15,10 +15,11 @@ public class PyRat {
         Point p = (new Point (0, 6));
         cheeses = fromages;
 
-        matrice = new boolean[labyWidth][labyHeight];
+        matriceFromages = new boolean[labyWidth][labyHeight];
+        matriceAtteint = new boolean[labyWidth][labyHeight];
 
         for (Point pf: fromages) {
-            matrice[pf.getX()][pf.getY()] = true;
+            matriceFromages[pf.getX()][pf.getY()] = true;
         }
 
         labyComplet = laby;
@@ -33,11 +34,11 @@ public class PyRat {
     public void turn(Map<Point, List<Point>> laby, int labyWidth, int labyHeight, Point position, List<Point> fromages) {
         Point pt1 = new Point(0,0);
         Point pt2 = new Point(1,5);
-        //System.out.println((fromageIci(pt1) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt1);
-        //System.out.println((fromageIci_EnOrdreConstant(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
+        System.out.println((fromageIci(pt1) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt1);
+        System.out.println((fromageIci_EnOrdreConstant(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
         System.out.println((passagePossible(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
-        //System.out.println((passagePossible_EnOrdreConstant(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
-        //System.out.println("Liste des points inatteignables depuis la position " + position + " : " + pointsInatteignables(position));
+        System.out.println((passagePossible_EnOrdreConstant(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
+        System.out.println("Liste des points inatteignables depuis la position " + position + " : " + pointsInatteignables(position));
     }
 
     /* Regarde dans la liste des fromages s’il y a un fromage à la position pos.
@@ -49,7 +50,7 @@ public class PyRat {
     /* Regarde de manière performante (accès en ordre constant) s’il y a un fromage à la position pos.
         @return true s'il y a un fromage à la position pos, false sinon. */
     private boolean fromageIci_EnOrdreConstant(Point pos) {
-        return matrice[pos.getX()][pos.getY()];
+        return matriceFromages[pos.getX()][pos.getY()];
     }
 
     /* Indique si le joueur peut passer de la position (du Point) « de » au point « a ».
@@ -69,13 +70,32 @@ public class PyRat {
     /* Retourne la liste des points qui ne peuvent pas être atteints depuis la position « pos ».
         @return la liste des points qui ne peuvent pas être atteints depuis la position « pos ». */
     private List<Point> pointsInatteignables(Point pos) {
-        List<Point> lpt = labyComplet.get(pos);
-        for (Point pt : lpt){
-            if (!pointAccessible.contains(pt)) {
-                pointAccessible.add(pt);
-                pointsInatteignables(pt);
+        List<Point> lstAtteint= new ArrayList<>();
+        List<Point> pointsAtteints = inatteignablesRec(pos, lstAtteint);
+        for (Point pa: pointsAtteints) {
+            matriceAtteint[pa.getX()][pa.getY()] = true;
+        }
+        List<Point> pasAtteint= new ArrayList<>();
+        for(int i=0; i<matriceAtteint.length; i++)
+        {
+            for(int j=0; j<matriceAtteint[i].length; j++)
+            {
+                if (matriceAtteint[i][j] == false) {
+                    pasAtteint.add(new Point(i,j));
+                }
             }
         }
-        return null;
+        return pasAtteint;
+    }
+
+    private List<Point> inatteignablesRec(Point pos, List<Point> lstAtteint) {
+        List<Point> listsPoint = labyComplet.get(pos);
+        for (Point pt : listsPoint) {
+            if (!lstAtteint.contains(pt)) {
+                lstAtteint.add(pt);
+                inatteignablesRec(pt, lstAtteint);
+            }
+        }
+        return lstAtteint;
     }
 }
